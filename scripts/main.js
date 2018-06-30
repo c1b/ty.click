cf796abe152d7b39cc43f736defbaff7d4a86537
c1b <c1b@users.noreply.github.com>
2018-06-29 21:41:25 -0700

    ty3

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,18 @@
 var myImage = document.querySelector('img');
 var x = 1;
 
-myImage.onclick = function() {
-    if(x === 1) {
-        myImage.setAttribute ('src','images/t2.jpg');
-        x = x + 1;
-    } else if (x === 2) {
-        myImage.setAttribute ('src','images/t3.jpg');
-        x = x + 1;
-    } else if (x === 3) {
-        myImage.setAttribute ('src','images/t4.jpg');
-        x = 1;
-    }
+myImage.onclick = function ()
+    if (x === 0) {
+  myImage.setAttribute('src', 'images/t1.jpg');
+  x = x + 1;
+} else if (x === 1) {
+  myImage.setAttribute('src', 'images/t2.jpg');
+  x = x + 1;
+} else if (x === 2) {
+  myImage.setAttribute('src', 'images/t3.jpg');
+  x = x + 1;
+} else if (x === 3) {
+  myImage.setAttribute('src', 'images/t4.jpg');
+  x = 0;
+}
 }
